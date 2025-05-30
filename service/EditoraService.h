cb6e05c8900b9d36af494c4e9ce07bb231f6d45c
Rannyer Gabriel de Oliveira <155459899+rannyeroliveira@users.noreply.github.com>
2025-05-30 18:05:15 -0300

    feat: Adiciona serviço para manipulação de editora: leitura e impressão de dados

--- service/EditoraService.h
+++ service/EditoraService.h
@@ -1,13 +1,88 @@
-#ifndef EDITORASERVICE_H
-#define EDITORASERVICE_H
+#ifndef EDITORA_SERVICE_H
+#define EDITORA_SERVICE_H
 
+#include "../model/Editora.h"
 #include "../repository/EditoraRepository.h"
+#include <iostream>
 
-class EditoraService {
+using namespace std;
+
+class EditoraService
+{
     EditoraRepository editoraRepository;
+
+
+
+
+
+public:
+    EditoraService() {}
+
+    Editora createEditora(int id, char nome[], int codigoCidade)
+    {
+        return Editora(id, nome, codigoCidade);
+    }
+
+    void createEditora()
+    {
+        int totalEditorasInserir;
+
+        cout << "Digite quantas editoras você deseja cadastrar: ";
+        cin >> totalEditorasInserir;
+
+        if (totalEditorasInserir > editoraRepository.getMaximoEditorasParaInserir())
+        {
+            while (totalEditorasInserir > editoraRepository.getMaximoEditorasParaInserir())
+            {
+                cout << "Tamanho máximo de memória do banco excedido! \nDigite outra quantidade: ";
+                cin >> totalEditorasInserir;
+            }
+        }
+
+        Editora tabelaNovasEditoras[totalEditorasInserir];
+
+        for (int i = 0; i < totalEditorasInserir; ++i)
+        {
+            int id;
+            char nome[100];
+            int codigoCidade;
+
+            cout << "Digite o ID: ";
+            cin >> id;
+
+            while (editoraRepository.existsByID(id))
+            {
+                cout << "ID " << id << " já utilizado. Digite um novo ID: ";
+                cin >> id;
+            }
+
+            cout << "Digite o nome da editora: ";
+            cin.ignore();
+            gets(nome);
+
+            cout << "Digite o código da cidade: ";
+            cin >> codigoCidade;
+
+            tabelaNovasEditoras[i].setId(id);
+            tabelaNovasEditoras[i].setNomeEdtr(nome);
+            tabelaNovasEditoras[i].setCodigoCidade(codigoCidade);
+        }
+
+        editoraRepository.inserirEditoras(tabelaNovasEditoras, totalEditorasInserir);
+    }
+
+    void deleteEditorasByID(int tabelaEditorasRemovidas[], int tamanhoTabela)
+    {
+        editoraRepository.deleteByID(tabelaEditorasRemovidas, tamanhoTabela);
+    }
+
+    void getByID(int id)
+    {
+        Editora e = editoraRepository.getByID(id);
+        Editora v[] = {e};
+
+    }
 
 };
 
-
-
-#endif //EDITORASERVICE_H
+#endif // EDITORA_SERVICE_H
